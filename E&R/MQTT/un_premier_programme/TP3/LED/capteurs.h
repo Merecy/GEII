5af794c0c42f299fc65fa911e5a6d69b7f22bbcc
Mereci <106228694+Merecy@users.noreply.github.com>
2022-05-25 09:41:48 +0200

    Delete E&R/MQTT/un_premier_programme/TP3 directory

--- E&R/MQTT/un_premier_programme/TP3/LED/capteurs.h
+++
@@ -1,5 +0,0 @@
-#ifndef CAPTEURS_H
-#define CAPTEURS_H
-void setup_capteurs();
-float lux();
-#endif
--- E&R/MQTT/un_premier_programme/TP3/LED/mqtt.h
+++
@@ -1,9 +0,0 @@
-#include <Arduino.h>
-#ifndef MQTT_H
-#define MQTT_H
-void setup_mqtt();
-void setup_wifi();
-void callback(char* topic, byte* payload, unsigned int length);
-void reconnect();
-void loop_mqtt();
-#endif
