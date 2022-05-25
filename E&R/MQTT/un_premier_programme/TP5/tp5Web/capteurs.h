85d2d05c23dc304b8b93b6e16edc27463f528a8e
Mereci <106228694+Merecy@users.noreply.github.com>
2022-05-25 09:41:58 +0200

    Delete E&R/MQTT/un_premier_programme/TP5 directory

--- E&R/MQTT/un_premier_programme/TP5/tp5Web/capteurs.h
+++
@@ -1,9 +0,0 @@
-#ifndef CAPTEURS_H
-#define CAPTEURS_H
-#define capteur 0x23
-#include <Wire.h>
-void setup_capteurs();
-void i2cWrite8(uint8_t data);
-void loop_capteurs();
-uint16_t lumina();
-#endif
